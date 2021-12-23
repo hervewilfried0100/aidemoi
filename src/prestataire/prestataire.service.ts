ee9ba526b0360282547986a423f0fc27ddde665b
Gnahore Herve <wilfried.gnahore@inexa-ci.com>
2021-12-23 07:58:02 +0000

    refactor: Réfactorisation de la fonctionnalité de suppression d'un prestataire

--- src/prestataire/prestataire.service.ts
+++ src/prestataire/prestataire.service.ts
@@ -111,6 +111,7 @@ export class PrestataireService {
       prestation: prestataire.prestation.label,
       dateCreation: prestataire.dateCreation
     }));
+    console.log("Prestataire", cleanPrestataire);
     return cleanPrestataire;
   }
 
